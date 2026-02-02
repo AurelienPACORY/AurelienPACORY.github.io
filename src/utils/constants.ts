4a9423bde1d33322e695d33cdbfc660ca508bde5
Aurélien Pacory <aurelien.pacory@gmail.com>
2026-02-02 16:03:47 +0100

    Fix: Correction du lien LinkedIn

--- src/utils/constants.ts
+++ src/utils/constants.ts
@@ -132,7 +132,7 @@ export const EXPERIENCES = [
 
 export const CONTACT_INFO = {
   email: 'aurelien.pacory@gmail.com',
-  linkedin: 'https://www.linkedin.com/in/aurelien-pacory-542164293',
+  linkedin: 'https://www.linkedin.com/in/aurélien-pacory-542164293',
   github: 'https://github.com/AurelienPACORY',
   location: 'France, Paris',
 };
